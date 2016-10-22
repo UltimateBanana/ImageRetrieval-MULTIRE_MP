dd6d065ee924bb2cd5b1c935d66575042af96461
Hannah Banana <hannahpatriciapalabay@Hannahs-MacBook-Pro.local>
2016-10-22 13:42:15 +0800

    Initial Files
    Contains:
    - 2530 images needed for the MP
    - MVC packages
    - GUI window in view package
    - cieConvert.java and Image.java in model package

---
+++ MULTIRE_ImageRetrieval/src/view/MainGUI.java
@@ -0,0 +1,44 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JFileChooser;
+
+public class MainGUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainGUI window = new MainGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 600, 450);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+	}
+}
